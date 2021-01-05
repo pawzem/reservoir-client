acf7423c844d29a7bfc0dc08a0f861f9cf911e7f
Paweł Żemła <pawzem@gmail.com>
2021-01-05 11:39:05 +0100

    routing

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # ReservoirClient
 
+Angular sandbox for pet project reservoir 
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.5.
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+Run `ng serve --open` to run and open browser
 
 ## Code scaffolding
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { OrganizationComponent } from './organization/organization.component';
+
+const routes = [
+  { path: '', component: OrganizationComponent },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: false,
+      enableTracing: false // for Debugging of the Routes
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
